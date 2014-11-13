348bb476509f15be33ca4b700f91d542815277b7
Bruno Sabot <brunosabot@gmail.com>
2014-11-13 11:19:52 +0100

    Update the task to merge the svg in a single file with defs

--- tasks/svgtodefs.js
+++ tasks/svgtodefs.js
@@ -8,43 +8,70 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('svgtodefs', 'Transform a list of svg files to a single defs file', function() {
+  grunt.registerMultiTask('svgtodefs', 'Transform a list of svg files to a single defs file', function () {
+
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      idPrefix: '',
+      svgClass: 'svg-defs'
     });
 
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
+    this.files.forEach(function (f) {
+
+      // Write the destination file.
+      var src = '<svg class="' + options.svgClass + '"><defs>';
+
       // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
+      src += f.src.filter(function (filepath) {
+
         if (!grunt.file.exists(filepath)) {
+
           grunt.log.warn('Source file "' + filepath + '" not found.');
           return false;
-        } else {
-          return true;
+
+        } else if (filepath.indexOf('.svg') === -1) {
+          return false;
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+
+        return true;
+
+      }).map(function (filepath) {
+
+        var fileContent = grunt.file.read(filepath);
+
+        fileContent = fileContent.replace(/<!.*>/gmi, '');
+        fileContent = fileContent.replace(/<\?.*\?>/gmi, '');
+        fileContent = fileContent.replace(/\s+/gmi, ' ');
+        fileContent = fileContent.replace(/<svg(.*?)>/gi, '');
+        fileContent = fileContent.replace(/<\/svg>/gmi, '');
+        fileContent = fileContent.replace(/<g(.*?)>/gi, '');
+        fileContent = fileContent.replace(/<\/g>/gmi, '');
+
+        var id = filepath.split('/');
+        id = id[id.length - 1];
+        id = id.replace(".svg", '');
+
+        return '<g id="' + options.idPrefix + id + '">' + fileContent.trim() + '</g>' + "\n";
+
+      }).join('');
 
       // Handle options.
-      src += options.punctuation;
+      src += '</defs></svg>';
 
       // Write the destination file.
       grunt.file.write(f.dest, src);
 
       // Print a success message.
       grunt.log.writeln('File "' + f.dest + '" created.');
+
     });
+
   });
 
 };
